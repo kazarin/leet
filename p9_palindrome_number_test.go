0ac38df00672d0d855cacbee9540c4277773b190
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-11-02 15:49:33 +0600

    Autoformat

--- p9_palindrome_number_test.go
+++ p9_palindrome_number_test.go
@@ -5,17 +5,16 @@ import (
 	"testing"
 )
 
-var tests = []struct {
-	input  int
-	output bool
-}{
-	{121, true},
-	{-121, false},
-	{10, false},
-	{1, true},
-	{313, true}}
-
 func TestIsPalindrome(t *testing.T) {
+	tests := []struct {
+		input  int
+		output bool
+	}{
+		{121, true},
+		{-121, false},
+		{10, false},
+		{1, true},
+		{313, true}}
 	for _, tt := range tests {
 		t.Run(fmt.Sprint(tt.input), func(t *testing.T) {
 			s := isPalindrome(tt.input)
