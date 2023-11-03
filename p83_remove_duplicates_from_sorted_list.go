5631300d0d8d54f70646f916bc4cb6b2f35f2a0e
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-11-03 14:01:50 +0600

    Do not fail if no list passed

--- p83_remove_duplicates_from_sorted_list.go
+++ p83_remove_duplicates_from_sorted_list.go
@@ -2,6 +2,9 @@ package main
 
 func deleteDuplicates(head *ListNode) *ListNode {
 	h := head
+	if h == nil {
+		return h
+	}
 	for h.Next != nil {
 		if h.Next.Val == h.Val {
 			h.Next = h.Next.Next
