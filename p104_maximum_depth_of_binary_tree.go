a371318e879cd1aa0dd7355067254104033eddc7
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-11-14 16:26:31 +0600

    Remove debug

--- p104_maximum_depth_of_binary_tree.go
+++ p104_maximum_depth_of_binary_tree.go
@@ -1,14 +1,11 @@
 package main
 
-import "fmt"
-
 func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 	l := maxDepth(root.Left)
 	r := maxDepth(root.Right)
-	fmt.Printf("l: %d, r: %d\n", l, r)
 	if l > r {
 		return l + 1
 	} else {
