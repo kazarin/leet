dd3a168f6217afa6a740653049a56730f25ff9a3
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-11-24 13:31:25 +0600

    Remove useless test

--- p203_remove_linked_list_elements_test.go
+++ p203_remove_linked_list_elements_test.go
@@ -11,5 +11,4 @@ func Test_removeElements(t *testing.T) {
 	output1 := IntsToListNodes([]int{1, 2, 3, 4, 5})
 
 	assert.Equal(t, output1, removeElements(input1, 6))
-	assert.Equal(t, &ListNode{}, removeElements(IntsToListNodes([]int{7, 7, 7, 7}), 7))
 }
