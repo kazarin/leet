31cc510725936e382989fa8d7faebcaa2357e1d7
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-11-27 16:23:34 +0600

    Solve p704

---
+++ p704_binary_search_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_search(t *testing.T) {
+	assert.Equal(t, 4, search([]int{-1, 0, 3, 5, 9, 12}, 9))
+	assert.Equal(t, -1, search([]int{-1, 0, 3, 5, 9, 12}, 2))
+	assert.Equal(t, -1, search([]int{-1, 0, 3, 5, 9, 12}, 13))
+}
