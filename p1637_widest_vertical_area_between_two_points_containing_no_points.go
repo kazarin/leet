9b509c25e242b301be7c33def3d8d360371f6432
Konstantin Kazarin <kazarin.konstantin@gmail.com>
2023-12-21 13:06:20 +0600

    Solve p1637

---
+++ p1637_widest_vertical_area_between_two_points_containing_no_points.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+func maxWidthOfVerticalArea(points [][]int) int {
+	sort.Slice(points, func(i, j int) bool { return points[i][0] < points[j][0] })
+
+	out := math.MinInt
+	for i := 0; i < len(points)-1; i++ {
+		out = max(out, points[i+1][0]-points[i][0])
+
+	}
+
+	return out
+}
